bbbc7baced664c4f7a4d3860a711181ee87a0038
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 15:52:28 +0800

    feat:使用数字内置构造函数

--- 05-literals-and-built-in-constructors/script.js
+++ 05-literals-and-built-in-constructors/script.js
@@ -14,3 +14,8 @@ console.log(Object.getPrototypeOf(strObj))
 
 console.log(strLit instanceof String)
 console.log(strObj instanceof String)
+
+const numLit = 42 // 数字字面量
+const numObj = new Number(42) // 数字对象
+console.log(numLit, typeof numLit) // 输出：42 number
+console.log(numObj, typeof numObj) // 输出：[Number: 42] object
