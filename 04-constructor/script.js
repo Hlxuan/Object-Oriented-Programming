5a973420a7e927be106ed2d2f43f42fabe83adc7
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 15:21:04 +0800

    feat:使用 Rectangle 构造函数来创建对象

--- 04-constructor/script.js
+++ 04-constructor/script.js
@@ -6,3 +6,9 @@ function Rectangle(name, width, height) {
   this.width = width // 分配width属性
   this.height = height // 分配height属性
 }
+
+const rect1 = new Rectangle("矩形 1", 10, 10)
+console.log(rect1)
+
+const rect2 = Rectangle("矩形 2", 20, 20)
+console.log(rect2)
