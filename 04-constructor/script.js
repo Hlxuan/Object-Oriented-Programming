d06bf39fad9fc4919007fcc2b3c24d2f21aab3c3
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 15:22:30 +0800

    update:向构造函数添加方法

--- 04-constructor/script.js
+++ 04-constructor/script.js
@@ -5,10 +5,15 @@ function Rectangle(name, width, height) {
   this.name = name // 分配name属性
   this.width = width // 分配width属性
   this.height = height // 分配height属性
+  this.area = function () {
+    return this.width * this.height
+  }
 }
 
 const rect1 = new Rectangle("矩形 1", 10, 10)
-console.log(rect1)
+console.log(rect1.area())
 
-const rect2 = Rectangle("矩形 2", 20, 20)
-console.log(rect2)
+const rect2 = new Rectangle("矩形 2", 20, 10)
+const rect3 = new Rectangle("矩形 3", 30, 30)
+console.log(rect2.name)
+console.log(rect3.area())
