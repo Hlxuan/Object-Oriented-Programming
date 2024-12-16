d2d6f645668bd9ea3929dca3b9ee4cdf020477a3
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 15:52:50 +0800

    feat:使用布尔值内置构造函数

--- 05-literals-and-built-in-constructors/script.js
+++ 05-literals-and-built-in-constructors/script.js
@@ -19,3 +19,8 @@ const numLit = 42 // 数字字面量
 const numObj = new Number(42) // 数字对象
 console.log(numLit, typeof numLit) // 输出：42 number
 console.log(numObj, typeof numObj) // 输出：[Number: 42] object
+
+const boolLit = true // 布尔字面量
+const boolObj = new Boolean(true) // 布尔对象
+console.log(boolLit, typeof boolLit) // 输出：true boolean
+console.log(boolObj, typeof boolObj) // 输出：[Boolean: true] object
