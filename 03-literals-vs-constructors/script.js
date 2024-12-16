9b79a95ddf02a901703549577f4c13c3ccc6dcb9
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 14:51:37 +0800

    update:对象字面量的局限性

--- 03-literals-vs-constructors/script.js
+++ 03-literals-vs-constructors/script.js
@@ -13,3 +13,22 @@ console.log(rectangle.area()) // 输出：200
 
 rectangle.width = 15
 console.log(rectangle.area()) // 输出：300
+
+const rectangle1 = {
+  name: "矩形 1",
+  width: 10,
+  height: 20,
+  area: function () {
+    return this.width * this.height
+  },
+}
+const rectangle2 = {
+  name: "矩形 2",
+  width: 30,
+  height: 40,
+  area: function () {
+    return this.width * this.height
+  },
+}
+console.log(rectangle1.area()) // 输出：200
+console.log(rectangle2.area()) // 输出：1200
