9d4640b90cbc327569c21c3167ec1242f79f7acf
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 15:20:11 +0800

    feat:创建构造函数

--- 04-constructor/script.js
+++ 04-constructor/script.js
@@ -1,2 +1,8 @@
 const now = new Date()
 console.log(now) // 输出当前日期和时间
+
+function Rectangle(name, width, height) {
+  this.name = name // 分配name属性
+  this.width = width // 分配width属性
+  this.height = height // 分配height属性
+}
