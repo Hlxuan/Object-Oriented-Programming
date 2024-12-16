f0b25858c0bc05ee02c1e17e29db78f7fbc9ffd9
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 14:49:46 +0800

    update:向对象字面量添加方法

--- 03-literals-vs-constructors/script.js
+++ 03-literals-vs-constructors/script.js
@@ -2,6 +2,14 @@ const rectangle = {
   name: "矩形 1",
   width: 10,
   height: 20,
+  area: function () {
+    return this.width * this.height
+  },
 }
 
 console.log(rectangle.name) // 输出：矩形 1
+
+console.log(rectangle.area()) // 输出：200
+
+rectangle.width = 15
+console.log(rectangle.area()) // 输出：300
