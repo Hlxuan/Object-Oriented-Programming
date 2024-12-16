891c916eedba65fa84656eea1e0645896c166d92
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-16 14:45:48 +0800

    feat:新增一个继承示例

--- 02-constructor-functions/script.js
+++ 02-constructor-functions/script.js
@@ -10,3 +10,25 @@ class Circle {
 
 const circle = new Circle(5)
 console.log(circle.area) // 使用 getter 访问面积
+
+class User {
+  sayHello() {
+    console.log("你好，我是用户。")
+  }
+}
+class Admin extends User {
+  sayHello() {
+    console.log("你好，我是管理员。")
+  }
+}
+class Guest extends User {
+  sayHello() {
+    console.log("你好，我是访客。")
+  }
+}
+const user = new User()
+const admin = new Admin()
+const guest = new Guest()
+user.sayHello() // 输出：你好，我是用户。
+admin.sayHello() // 输出：你好，我是管理员。
+guest.sayHello() // 输出：你好，我是访客。
