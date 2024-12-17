0dbea1fcd529eb01cea7327174271e541150b6f3
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-17 08:55:35 +0800

    update:使用点符号或括号符号来访问属性

--- 06-object-properties/script.js
+++ 06-object-properties/script.js
@@ -1,8 +1,16 @@
 function Rectangle(name, width, height) {
-  this.name = name;
-  this.width = width;
-  this.height = height;
+  this.name = name
+  this.width = width
+  this.height = height
   this.area = function () {
-    return this.width * this.height;
-  };
+    return this.width * this.height
+  }
 }
+
+const rect1 = new Rectangle("矩形 1", 20, 20)
+const rect2 = new Rectangle("矩形 2", 30, 20)
+
+console.log(rect1.name)
+console.log(rect2.width)
+
+console.log(rect1["width"])
