036e869736370c152fd95db32ea6018a6edb0c15
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-23 09:28:19 +0800

    update:更新构造函数,area在原型中

--- 08-add-prototype-methods/script.js
+++ 08-add-prototype-methods/script.js
@@ -1,13 +1,13 @@
 function Rectangle(name, width, height) {
-  this.name = name;
-  this.width = width;
-  this.height = height;
-  this.area = function () {
-  	return this.width * this.height;
-  };
+  this.name = name
+  this.width = width
+  this.height = height
 }
 
+Rectangle.prototype.area = function () {
+  return this.width * this.height
+}
 
-const rect = new Rectangle('Rect', 10, 10);
+const rect = new Rectangle("Rect", 10, 10)
 
-console.log(rect);
+console.log(rect.area())
