f301701879ca2168987f361e8fcf85d86a78014f
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-23 10:28:09 +0800

    feat:创建 Rectangle 和 Circle 构造函数

--- 10-prototypical-inheritance-call/script.js
+++ 10-prototypical-inheritance-call/script.js
@@ -1,3 +1,14 @@
 function Shape(name) {
   this.name = name
 }
+
+function Rectangle(name, width, height) {
+  Shape.call(this, name)
+  this.width = width
+  this.height = height
+}
+
+function Circle(name, radius) {
+  Shape.call(this, name)
+  this.radius = radius
+}
