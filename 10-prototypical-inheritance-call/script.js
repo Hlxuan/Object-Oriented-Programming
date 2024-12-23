587e5582d0321fbeb17f7a079d5b474a8c0eace8
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-23 10:35:00 +0800

    update:重写原型方法（多态）

--- 10-prototypical-inheritance-call/script.js
+++ 10-prototypical-inheritance-call/script.js
@@ -26,5 +26,12 @@ Shape.prototype.logName = function () {
 
 const rect = new Rectangle("矩形 1", 20, 30)
 const circle = new Circle("圆形 1", 15)
+// rect.logName()
+// circle.logName()
+
+Rectangle.prototype.logName = function () {
+  console.log(`矩形
+    名称：${this.name}`)
+}
 rect.logName()
 circle.logName()
